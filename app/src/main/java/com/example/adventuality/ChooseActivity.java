848478d9b67ae213025d9445fcd39563bdcb0b91
Joshua Minton <joshuamntn@gmail.com>
2020-12-17 13:52:09 +0000

    Bug fix -- refreshing chosen devotionals when returning from details page having changed status.

--- app/src/main/java/com/example/adventuality/ChooseActivity.java
+++ app/src/main/java/com/example/adventuality/ChooseActivity.java
@@ -140,6 +140,7 @@ public class ChooseActivity extends AppCompatActivity implements DevotionalAdapt
                 dev.setFollowed(false);
             }
         }
+        devotionalsAdapter.notifyDataSetChanged();
     }
 
 }
